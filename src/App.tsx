ace9132fcfa9b66de67e5b1cc368a2a62bd6d899
CtrlAltDelight <lbchigges@gmail.com>
2023-08-18 10:31:55 -0400

    Added more example componenets, like Toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import Alert from "./components/Alert";
+import {useState} from "react";
 
 function App() {
-  let items = ["New York", "London", "Beijing", "Paris", "Delhi"];
-
-  // event handler
-	const handleSelectItem = (item: string) => {
-		console.log(item);
-	}
+	const [alertVisible, setAlertVisible] = useState(false)
 
   return (
-    <div>
-		<Alert>
-			Hello <span>World</span>
-		</Alert>
-    </div>
+	  <>
+	<div className="container-sm">100% wide until small breakpoint</div>
+	<div className="container-md">100% wide until medium breakpoint</div>
+	<div className="container-lg">100% wide until large breakpoint</div>
+	<div className="container-xl">100% wide until extra large breakpoint</div>
+	<div className="container-xxl">100% wide until extra extra large breakpoint</div>
+	</>
   );
   // Self closing syntax: <Message />
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,13 +1,25 @@
 import { ReactNode } from "react";
 
 interface Props {
-	children: ReactNode;
+  children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-	return (
-	<div className="alert alert-primary">{children}</div>
-	);
+const Alert = ({ children, onClose }: Props) => {
+  // Handler for dismiss button
+  const handleButtonPress = () => {
+    console.log("nothing");
+  };
+
+  return (
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      {children}
+	 <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={onClose}></button>
+    </div>
+  );
 };
 
-export default Alert
+export default Alert;
