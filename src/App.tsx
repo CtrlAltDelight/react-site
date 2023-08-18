ad3dd4c302d886b6475bbd4fe819071fc1155774
CtrlAltDelight <lbchigges@gmail.com>
2023-08-18 09:06:49 -0400

    Added some example components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import Message from './Message';
+import Alert from "./components/Alert";
 
 function App() {
-	return <div><Message></Message></div>;
-	// or use self closing syntax: <Message />
+  let items = ["New York", "London", "Beijing", "Paris", "Delhi"];
+
+  // event handler
+	const handleSelectItem = (item: string) => {
+		console.log(item);
+	}
+
+  return (
+    <div>
+		<Alert>
+			Hello <span>World</span>
+		</Alert>
+    </div>
+  );
+  // Self closing syntax: <Message />
 }
 
 export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+// Props are essentially parameters for the react component
+// Props are immutable (read-only!)
+interface Props {
+	items: string[];
+	heading: string;
+
+	onSelectItem: (item: string) => void; // kind of like onClick
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // Hook
+  // State is mutable, similar to local variables, and managed by component
+  // Both state and props will cause a re-render when changed.
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // uses this syntax: const [name, setName] = useState('');
+
+  // <> tells react to use a fragment
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+			  onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
